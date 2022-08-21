c3ac684af0def9594988c97af68826b150d16293
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2022-08-21 18:34:24 -0400

    added project files

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const os = require('os');
+const app = express();
+
+app.use('/', async (req, res) => {
+  console.log(`I am sending a response ${os.hostname()}`)
+  res.json({message:'Ok It works...', hostname: os.hostname()})
+});
+
+app.listen(3000,()=> console.log('Server on port 3000'));
